b4fb5cc4e81053c2d1b0f415676d41dfd84374fc
Fbisinger <37881821+Fbisinger@users.noreply.github.com>
2018-05-16 20:43:54 +0200

    Add files via upload

---
+++ Taupunkt.cpp
@@ -0,0 +1,40 @@
+/* ============================================
+Arduino Taupunkt-Bibliothek
+           2018 F.Bisinger
+
+Diese Bibliothek errechnet aus Temperatur und Luftfeuchtigkeitswerten(relativ)
+den Taupunkt.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+===============================================
+*/
+#include "Taupunkt.h"
+
+Taupunkt::Taupunkt() { 
+}
+
+float Taupunkt::getTaupunkt(float Temperatur, float Luftfeuchtigkeit) {
+
+    _Taupunkt = (234.67*0.434292289*log(6.1*exp((7.45*Temperatur)/(234.67+Temperatur)*2.3025851)*Luftfeuchtigkeit/100/6.1))/(7.45-0.434292289*log(6.1*exp((7.45*Temperatur)/(234.67+Temperatur)*2.3025851)*Luftfeuchtigkeit/100/6.1));
+
+ return _Taupunkt;
+}
+
+
+
---
+++ Taupunkt_Demo/Taupunkt_Demo.ino
@@ -0,0 +1,34 @@
+/* ============================================
+Arduino Taupunkt-Bibliothek
+           2018 F.Bisinger
+
+Diese Bibliothek errechnet aus Temperatur und 
+Luftfeuchtigkeitswerten(relativ) den Taupunkt.
+===============================================
+*/
+#include "Taupunkt.h"
+Taupunkt demo;  //kann beliebig benannt werden.
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+Serial.println(demo.getTaupunkt(18,73));  //mit demo.getTaupunkt(Temperatur,Luftfeuchtigkeit) wird der Taupunkt errechnet, und als float ausgegeben.
+delay(500);
+}
+
+/* Zusätzliche Informationen zum Taupunkt (Quelle: https://de.wikipedia.org/wiki/Taupunkt)
+ * -------------------------------------------------------------------------------------------------------------------- |
+ * Der Taupunkt, auch die Taupunkttemperatur, ist bei Luft mit einer bestimmten Luftfeuchtigkeit diejenige Temperatur,  |
+ * die bei konstantem Druck unterschritten werden muss, damit sich Wasserdampf als Tau oder Nebel abscheiden kann.      |
+ * Am Taupunkt beträgt die relative Luftfeuchtigkeit 100 % und die Luft ist mit Wasserdampf (gerade) gesättigt.         |
+ * Je mehr Wasserdampf die Luft enthält, desto höher liegt deren Taupunkttemperatur. Der Taupunkt kann daher zur        |
+ * Bestimmung der absoluten Luftfeuchtigkeit dienen. Der Taupunkt wird mit einem Taupunktspiegelhygrometer direkt,      |
+ * oder mit anderen hygrometrischen Verfahren indirekt gemessen. Der Begriff des Taupunkts wird sinngemäß auch auf      |
+ * andere Gasgemische mit kondensierbaren Bestandteilen angewendet. Für die Taupunkttemperatur wird oft der griechische |
+ * Buchstabe τ als Formelzeichen verwendet.                                                                             |
+ * ---------------------------------------------------------------------------------------------------------------------|
+ */
+
+
